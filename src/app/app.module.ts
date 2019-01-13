b7f90b50ae5fe72d946179de5c3c0f73cb5352b1
Huiren Woo <giantcrabby@gmail.com>
2019-01-13 14:48:40 +0800

    Add shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
+import {SharedModule} from './shared/shared.module';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 
@@ -12,7 +13,8 @@ import {HomeComponent} from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
