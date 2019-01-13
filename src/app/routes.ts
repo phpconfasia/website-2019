c8f5827f5979d8488cca2cbf3358b0a347c9a837
Huiren Woo <giantcrabby@gmail.com>
2019-01-13 14:55:53 +0800

    Add home component route

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,3 +1,12 @@
 import {Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: '',
+  pathMatch: 'full',
+  component: HomeComponent,
+  data: {
+    title: 'Home | PHPConfAsia 2019',
+    description: 'Welcome to PHPConfAsia 2019! Our theme for this year is Framework Wars!'
+  },
+}];
