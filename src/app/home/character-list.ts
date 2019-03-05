c1c883d08162f7ea6534515cda04ac8c695b9de4
Huiren Woo <giantcrabby@gmail.com>
2019-03-05 10:43:49 +0000

    Add characters system

---
+++ src/app/home/character-list.ts
@@ -0,0 +1,32 @@
+import {Character} from './character';
+
+export const characterList: Character[] = [
+    {
+        name: 'Laravel',
+        description: 'Although much slower than the rest, their tools and heavy armor have been proven useful for ages.',
+        characterAvatar: '',
+        characterImg: '',
+        characterBg: ''
+    },
+    {
+        name: 'Zend',
+        description: 'An armed rifle symbolizing enterprise and their riches.',
+        characterAvatar: '',
+        characterImg: '',
+        characterBg: ''
+    },
+    {
+        name: 'Symfony',
+        description: 'Bard with an ability to compose code through magical music notes. Symfony is a framework that embodies elegance.',
+        characterAvatar: '',
+        characterImg: '',
+        characterBg: ''
+    },
+    {
+        name: 'Framework-less',
+        description: 'A swift ninja without burdens of a framework. They\'re able strike swiftly using effective and light weight code.',
+        characterAvatar: '',
+        characterImg: '',
+        characterBg: ''
+    }
+];
