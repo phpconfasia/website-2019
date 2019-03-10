ca6c5fc406b39c6535027cd24bfc0b3e38734485
Huiren Woo <giantcrabby@gmail.com>
2019-03-11 01:51:05 +0800

    Add class selector for framework

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
 import {Character} from './character';
 import {characterList} from './character-list';
@@ -10,19 +10,19 @@ import {characterList} from './character-list';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent implements OnInit {
-  public chosenCharacter$: BehaviorSubject<Character>;
+  public selectedCharacter$: BehaviorSubject<Character>;
   public characterList: Character[] = characterList;
 
   constructor() {
-    this.chosenCharacter$ = new BehaviorSubject<Character>(this.characterList[0]);
+    this.selectedCharacter$ = new BehaviorSubject<Character>(this.characterList[0]);
   }
 
   ngOnInit() {
   }
 
   public selectCharacter(selectedIndex: number): void {
-    if(selectedIndex >= 0 && selectedIndex < this.characterList.length) {
-      this.chosenCharacter$.next(this.characterList[selectedIndex]);
+    if (selectedIndex >= 0 && selectedIndex < this.characterList.length) {
+      this.selectedCharacter$.next(this.characterList[selectedIndex]);
     }
   }
 }
