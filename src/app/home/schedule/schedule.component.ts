63755fe3f9d31ed0d72e2eb2f6290120bb114f42
Huiren Woo <giantcrabby@gmail.com>
2019-03-24 19:56:49 +0800

    Add basic 'happening now'

--- src/app/home/schedule/schedule.component.ts
+++ src/app/home/schedule/schedule.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, Observable, Observer} from 'rxjs';
 import {ScheduleEvent} from './schedule-event';
 import {scheduleEventList} from './schedule-event-list';
 
@@ -12,6 +12,7 @@ import {scheduleEventList} from './schedule-event-list';
 export class ScheduleComponent implements OnInit {
   public selectedDay$: BehaviorSubject<Number>;
   public scheduleEventList: Array<ScheduleEvent[]> = scheduleEventList;
+  public dateNow: Date = new Date();
 
   constructor() {
     this.selectedDay$ = new BehaviorSubject<Number>(this.getCurrentDay());
@@ -31,9 +32,7 @@ export class ScheduleComponent implements OnInit {
   }
 
   public getCurrentDay(): number {
-    const dateNow = new Date();
-
-    switch (dateNow.getDate()) {
+    switch (this.dateNow.getDate()) {
       case 25:
         return 1;
       case 26:
@@ -44,4 +43,21 @@ export class ScheduleComponent implements OnInit {
     }
   }
 
+  public getCurrentScheduleEvent(): Observable<ScheduleEvent | null> {
+    return new Observable((observer: Observer<ScheduleEvent | null>) => {
+      const scheduleEventDay: ScheduleEvent[] = scheduleEventList[this.getCurrentDay()];
+      for (let i = 0; i < scheduleEventDay.length; i++) {
+        const scheduleEvent = scheduleEventDay[i];
+
+        if (scheduleEvent.dateTime.getTime() <= this.dateNow.getTime() && scheduleEvent.dateTimeEnd.getTime() >= this.dateNow.getTime()) {
+          observer.next(scheduleEvent);
+          observer.complete();
+          return;
+        }
+      }
+
+      observer.next(null);
+      observer.complete();
+    });
+  }
 }
