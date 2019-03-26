a65fd52eabaa881abc32e9ca082f05f21c6714b4
Huiren Woo <giantcrabby@gmail.com>
2019-03-26 19:20:40 +0800

    Add some sponsoring companies

--- src/app/home/sponsor-list.ts
+++ src/app/home/sponsor-list.ts
@@ -2,6 +2,14 @@ import {Sponsor} from './sponsor';
 
 export const sponsorList: { [rank: string]: Sponsor[] } = {
   'gold': [],
-  'silver': [],
-  'supporter': []
+  'silver': [
+    {
+      name: 'Microsoft Singapore',
+      description: '',
+      sponsorTitle: 'Venue Sponsor',
+      imgUrl: '/assets/img/sponsoring-companies/microsoft.png'
+    }
+  ],
+  'supporter': [],
+  'community': []
 };
